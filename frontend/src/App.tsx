cdbb534fffb5c3972c0a9e29d3caafda98ab0e0c
Jose Guilherme Benites Machado <jgbm81@hotmail.com>
2022-11-15 16:24:43 -0400

    Others Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"> </div>
+          <SalesCard />
+        </section>
+      </main>
     </>
   )
 }
